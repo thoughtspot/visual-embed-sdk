b0f76a696cb8afc17e62d32a261ef56edb9b00c1
Pankaj Pandey <pankaj.pandey@thoughtspot.com>
2021-02-03 22:37:17 +0530

    - html parser files

---
+++ docs/src/utils/app-utils.js
@@ -0,0 +1,12 @@
+export const queryStringParser = (queryParamStr) => {
+    const queryParamObj = {};
+    if (!queryParamStr) return queryParamObj;
+
+    const entries = new URLSearchParams(queryParamStr).entries();
+    for (const [key, value] of entries) {
+        queryParamObj[key] = value;
+    }
+    return queryParamObj;
+};
+
+export default queryStringParser;
---
+++ docs/src/utils/doc-utils.js
@@ -0,0 +1,22 @@
+export const htmlParser = (html, params) => {
+    let parsedHtml = html;
+    const paramKeys = Object.keys(params);
+    if (!html && paramKeys.length === 0) return parsedHtml;
+    const customPassThroughStart = '{{';
+    const customPassThroughEnd = '}}';
+
+    paramKeys.map((key) => {
+        console.log('key = ', key, params[key]);
+        parsedHtml = parsedHtml.replace(
+            new RegExp(
+                `${customPassThroughStart}${key}${customPassThroughEnd}`,
+                'g',
+            ),
+            params[key] || '',
+        );
+    });
+
+    return parsedHtml;
+};
+
+export default htmlParser;
