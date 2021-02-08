962000ba4d0ad2171d1c1a9aa3ae5633ca0c5c6a
Harsh <harsh.rathi@thoughtspot.com>
2021-02-08 16:09:43 +0530

    - added variable for header z-index and added style for 404 component

---
+++ docs/src/components/Page404/index.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import { Link } from 'gatsby';
+import './index.scss';
+
+const Page404 = () => {
+    return (
+        <main className="pageStyles">
+            <title>Not found</title>
+            <h1 className="headingStyles">Page not found</h1>
+            <p className="paragraphStyles">
+                Sorry{' '}
+                <span role="img" aria-label="Pensive emoji">
+                    😔
+                </span>{' '}
+                we couldn’t find what you were looking for.
+                <br />
+                {process.env.NODE_ENV === 'development' ? (
+                    <>
+                        <br />
+                        Try creating a page in{' '}
+                        <code className="codeStyles">src/pages/</code>.
+                        <br />
+                    </>
+                ) : null}
+                <br />
+                <Link to="/">Go home</Link>.
+            </p>
+        </main>
+    );
+};
+
+export default Page404;
