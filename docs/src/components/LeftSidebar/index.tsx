4100751e74053d2b027716a58c5f4c7c701ea661
Pankaj Pandey <pankaj.pandey@thoughtspot.com>
2021-02-10 19:59:01 +0530

    - integration of adoc files for content and navigation

--- docs/src/components/LeftSidebar/index.tsx
+++ docs/src/components/LeftSidebar/index.tsx
@@ -1,32 +1,18 @@
 import React from 'react';
 import './index.scss';
-import data from './data';
 import BackButton from '../BackButton';
 
-const LeftSideBar = () => (
+const LeftSideBar = (props: { navTitle: string; navContent: string }) => (
     <aside>
         <BackButton title="SpotDev Home" />
         <nav>
-            <h2 className="heading">Documentation</h2>
-            {data.map((content) => (
-                <div className="navWrapper" key={content.title}>
-                    <h3 className="subHeading">{content.title}</h3>
-                    <ul>
-                        {content.links.map((link) => (
-                            <li className="navlinks" key={link.label}>
-                                <a
-                                    href="#"
-                                    className={`navlinksAnchor ${
-                                        link.isActive ? 'activelink' : ''
-                                    }`}
-                                >
-                                    {link.label}
-                                </a>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            ))}
+            <h2 className="heading">{props.navTitle}</h2>
+            <div
+                className="navWrapper"
+                dangerouslySetInnerHTML={{
+                    __html: props.navContent,
+                }}
+            />
         </nav>
     </aside>
 );
--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useStaticQuery, graphql } from 'gatsby';
+import { useStaticQuery, graphql, navigate } from 'gatsby';
 import queryStringParser from '../utils/app-utils';
 import passThroughHandler from '../utils/doc-utils';
 import Docmap from '../components/Docmap';
@@ -11,19 +11,40 @@ import '../assets/styles/index.scss';
 
 // markup
 const IndexPage = ({ location }) => {
-    const [params, setParams] = useState({});
+    const [params, setParams] = useState({ tshost: '', pageid: '' });
     const [docTitle, setDocTitle] = useState('');
     const [docContent, setDocContent] = useState('');
+    const [navTitle, setNavTitle] = useState('');
+    const [navContent, setNavContent] = useState('');
 
     useEffect(() => {
         setParams(queryStringParser(location.search));
     }, []);
 
     useEffect(() => {
-        setDocTitle(
-            edges[0].node.document.title || edges[0].node.document.main,
+        const navIndex = edges.findIndex(
+            (i) => i.node.pageAttributes.pageid === 'nav',
         );
-        setDocContent(passThroughHandler(edges[0].node.html, params));
+        setNavTitle(edges[navIndex].node.pageAttributes.title);
+        setNavContent(passThroughHandler(edges[navIndex].node.html, params));
+
+        if (params.pageid) {
+            const edgeIndex = edges.findIndex(
+                (i) => i.node.pageAttributes.pageid === params.pageid,
+            );
+
+            if (edgeIndex > -1) {
+                setDocTitle(
+                    edges[edgeIndex].node.document.title ||
+                        edges[edgeIndex].node.pageAttributes.title,
+                );
+                setDocContent(
+                    passThroughHandler(edges[edgeIndex].node.html, params),
+                );
+            } else {
+                navigate('/404');
+            }
+        }
     }, [params]);
 
     const {
@@ -34,11 +55,13 @@ const IndexPage = ({ location }) => {
                 allAsciidoc(sort: { fields: [document___title], order: ASC }) {
                     edges {
                         node {
-                            id
                             document {
                                 title
-                                subtitle
-                                main
+                            }
+                            pageAttributes {
+                                pageid
+                                title
+                                description
                             }
                             html
                         }
@@ -52,7 +75,7 @@ const IndexPage = ({ location }) => {
         <>
             <Header />
             <main>
-                <LeftSidebar />
+                <LeftSidebar navTitle={navTitle} navContent={navContent} />
                 <div className="documentBody">
                     <Search />
                     <div className="introWrapper">
