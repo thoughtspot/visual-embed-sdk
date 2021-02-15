f2713aa1885dbd0bc9a6f0a7369931e6d22e9f83
Harsh <harsh.rathi@thoughtspot.com>
2021-02-15 18:12:22 +0530

    - Removed search component, added constants for font-weight and added comments

--- docs/src/components/Docmap/index.tsx
+++ docs/src/components/Docmap/index.tsx
@@ -6,6 +6,9 @@ const Docmap = (props: { docContent: string }) => {
     const [toc, setToc] = useState('');
 
     useEffect(() => {
+        // GraphQL doesn't provide any seperate html for Table of Content. It is included in the document itself.
+        // To extract the TOC from document, we first create a temporary element to set the document as it's innerHTML.
+        // Them we search for TOC using querySelector on the temporary element and then set the obtained TOC to display in the UI.
         const doc = document.createElement('div');
         doc.innerHTML = props.docContent;
         const tocEl = doc.querySelector('#toc');
