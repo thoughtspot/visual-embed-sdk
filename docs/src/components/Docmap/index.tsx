d756811f3b1eb61b7b957fa50650fef7a0de1444
Pankaj Pandey <pankaj.pandey@thoughtspot.com>
2021-02-15 18:39:53 +0530

    Squashed commit of the following:
    commit 169930e4d276a7607bed3940271eb9084c66f9fe
    Author: harsh-ts <78337774+harsh-ts@users.noreply.github.com>
    Date:   Mon Feb 15 18:35:12 2021 +0530
    
        [SCAL-77263] Dynamic right nav implementation  (#37)
    
        * - Added dynamic TOC
    
        * - Added scroll bar to right nav
    
        * - Removed duplicate styling from right nav
    
        * - Removed unused style
    
        * - Added checks for toc
    
        * - Removed search component, added constants for font-weight and added comments

--- docs/src/components/Docmap/index.tsx
+++ docs/src/components/Docmap/index.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './index.scss';
 
-const Docmap = () => (
-    <div className="docmapLinks">
-        <ul>
-            <li>
-                <a href="#" className="defaultStyle visited">
-                    On this Page
-                </a>
-            </li>
-            <li>
-                <a href="#" className="defaultStyle active">
-                    Rights and Obligations
-                </a>
-            </li>
-            <li>
-                <a href="#" className="defaultStyle">
-                    Section Contents
-                </a>
-            </li>
-        </ul>
-    </div>
-);
+const Docmap = (props: { docContent: string }) => {
+
+    const [toc, setToc] = useState('');
+
+    useEffect(() => {
+        // GraphQL doesn't provide any seperate html for Table of Content. It is included in the document itself.
+        // To extract the TOC from document, we first create a temporary element to set the document as it's innerHTML.
+        // Them we search for TOC using querySelector on the temporary element and then set the obtained TOC to display in the UI.
+        const doc = document.createElement('div');
+        doc.innerHTML = props.docContent;
+        const tocEl = doc.querySelector('#toc');
+        if (tocEl) {
+            setToc(tocEl.innerHTML);
+        } else {
+            setToc('');
+        }
+    }, [props.docContent]);
+
+    return (
+        <div className="docmapLinks">
+            {
+                toc !== ''
+                &&
+                <>
+                    <p className="tocTitle">On this page</p>
+                    <div
+                        dangerouslySetInnerHTML={{ __html: toc }}
+                    />
+                </>
+            }
+        </div>
+    );
+};
 
 export default Docmap;
