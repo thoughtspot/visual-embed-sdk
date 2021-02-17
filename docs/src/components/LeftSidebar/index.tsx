316ae392412dfdaf172e27cf404d40c1eb4ee53b
Harsh <harsh.rathi@thoughtspot.com>
2021-02-17 14:32:32 +0530

    - Used react-resizable to make left nav resizable

--- docs/src/components/LeftSidebar/index.tsx
+++ docs/src/components/LeftSidebar/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ResizableBox } from 'react-resizable';
 import './index.scss';
 import BackButton from '../BackButton';
 
@@ -6,21 +7,33 @@ const LeftSideBar = (props: {
     navTitle: string;
     navContent: string;
     backLink: string;
-}) => (
-    <aside>
-        {props.backLink && (
-            <BackButton title="SpotDev Home" backLink={props.backLink} />
-        )}
-        <nav>
-            <h2 className="heading">{props.navTitle}</h2>
-            <div
-                className="navWrapper"
-                dangerouslySetInnerHTML={{
-                    __html: props.navContent,
-                }}
-            />
-        </nav>
-    </aside>
-);
+}) => {
+
+    return (
+        <ResizableBox
+            width={310}
+            minConstraints={[100, 100]}
+            maxConstraints={[310]}
+            axis="x"
+            style={{ position: 'fixed' }}
+            height={window.screen.height}
+        >
+            <aside>
+                {props.backLink && (
+                    <BackButton title="SpotDev Home" backLink={props.backLink} />
+                )}
+                <nav>
+                    <h2 className="heading">{props.navTitle}</h2>
+                    <div
+                        className="navWrapper"
+                        dangerouslySetInnerHTML={{
+                            __html: props.navContent,
+                        }}
+                    />
+                </nav>
+            </aside>
+        </ResizableBox>
+    );
+};
 
 export default LeftSideBar;
