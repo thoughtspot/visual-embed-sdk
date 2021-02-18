ec717251ec931d0ff3c73957fd590f34fcb07108
Pankaj Pandey <pankaj.pandey@thoughtspot.com>
2021-02-19 00:21:02 +0530

    - added doc repo name config var

--- docs/src/configs/doc-configs.js
+++ docs/src/configs/doc-configs.js
@@ -1,18 +1,10 @@
-const DOC_REPO_NAME = '/embed-sdk';
-const DOC_NAV_PAGE_ID = 'nav';
-const TS_HOST_PARAM = 'tshost';
-const TS_ORIGIN_PARAM = 'origin';
-const TS_PAGE_ID_PARAM = 'pageid';
-const NAV_PREFIX = 'navprefix';
-const NOT_FOUND_PAGE_ID = '404-error';
-const TYPE_DOC_PREFIX = 'typedoc';
-
 module.exports = {
-    DOC_NAV_PAGE_ID,
-    TS_HOST_PARAM,
-    TS_ORIGIN_PARAM,
-    TS_PAGE_ID_PARAM,
-    NAV_PREFIX,
-    NOT_FOUND_PAGE_ID,
-    TYPE_DOC_PREFIX,
+    DOC_REPO_NAME: '/embed-sdk',
+    DOC_NAV_PAGE_ID: 'nav',
+    TS_HOST_PARAM: 'tshost',
+    TS_ORIGIN_PARAM: 'origin',
+    TS_PAGE_ID_PARAM: 'pageid',
+    NAV_PREFIX: 'navprefix',
+    NOT_FOUND_PAGE_ID: '404-error',
+    TYPE_DOC_PREFIX: 'typedoc',
 };
