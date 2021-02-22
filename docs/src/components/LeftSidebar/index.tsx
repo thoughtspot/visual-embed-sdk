8c440e47fe901152af713855d5295d9b902062e3
sumit-thoughtspot <78427342+sumit-thoughtspot@users.noreply.github.com>
2021-02-22 15:56:45 +0530

    - Added left side bar active link as per url (#60)

--- docs/src/components/LeftSidebar/index.tsx
+++ docs/src/components/LeftSidebar/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './index.scss';
 import BackButton from '../BackButton';
 
@@ -6,21 +6,38 @@ const LeftSideBar = (props: {
     navTitle: string;
     navContent: string;
     backLink: string;
-}) => (
-    <aside>
-        {props.backLink && (
-            <BackButton title="SpotDev Home" backLink={props.backLink} />
-        )}
-        <nav>
-            <h2 className="heading">{props.navTitle}</h2>
-            <div
-                className="navWrapper"
-                dangerouslySetInnerHTML={{
-                    __html: props.navContent,
-                }}
-            />
-        </nav>
-    </aside>
-);
+}) => {
+
+    const [pageid] = useState(location.search.slice(1).split('&')[0]);
+    const [navContent, setNavContent] = useState('');
+    
+    useEffect(() => {
+        const divElement = document.createElement('div');
+        divElement.innerHTML = props.navContent;
+        const tag = divElement.querySelector(`a[href='?${pageid}']`);
+        if (tag) {
+            tag.classList.add('active');
+            tag.parentElement.classList.add('activelink');
+        }
+        setNavContent(divElement.innerHTML);
+    }, [pageid, props.navContent]);
+
+    return (
+        <aside>
+            {props.backLink && (
+                <BackButton title='SpotDev Home' backLink={props.backLink} />
+            )}
+            <nav>
+                <h2 className='heading'>{props.navTitle}</h2>
+                <div
+                    className='navWrapper'
+                    dangerouslySetInnerHTML={{
+                        __html: navContent,
+                    }}
+                />
+            </nav>
+        </aside>
+    );
+};
 
 export default LeftSideBar;
