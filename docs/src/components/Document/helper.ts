a59b97374d72570599df6d58a9c206adae9b9062
Harsh <harsh.rathi@thoughtspot.com>
2021-02-26 04:26:28 +0530

    - Added comments and cleaned up code

--- docs/src/components/Document/helper.ts
+++ docs/src/components/Document/helper.ts
@@ -24,27 +24,26 @@ export const addEventListner = (element: HTMLElement, ...args: HTMLElement[]) =>
 };
 
 export const customizeDocContent = () => {
-    const allCodeTags = document.querySelectorAll('.listingblock>.content>.highlight>code');
-    if (allCodeTags.length > 0) {
-        document.querySelectorAll('.listingblock>.content>.highlight>code').forEach((tag) => {
-            const buttonElement = document.createElement('button');
-            buttonElement.setAttribute('class', 'copyButton');
-            addEventListner(buttonElement, tag as HTMLElement);
-            const imageElement = document.createElement('img');
-            imageElement.src = Copy;
-            imageElement.alt = 'Copy';
-            imageElement.title = 'Copy';
-            buttonElement.appendChild(imageElement);
-            const spanElement = document.createElement('span');
-            spanElement.innerText = tag.getAttribute('data-lang');
-            spanElement.classList.add('lang');
-            const wrapperDiv = document.createElement('div');
-            wrapperDiv.classList.add('wrapperContainer');
-            wrapperDiv.appendChild(spanElement);
-            wrapperDiv.appendChild(buttonElement);
-            tag.parentElement.appendChild(wrapperDiv);
-        });
-    }
+    /* To get all the code blocks from document */
+    document.querySelectorAll('.listingblock>.content>.highlight>code').forEach((tag) => {
+        const buttonElement = document.createElement('button');
+        buttonElement.setAttribute('class', 'copyButton');
+        addEventListner(buttonElement, tag as HTMLElement);
+        const imageElement = document.createElement('img');
+        imageElement.src = Copy;
+        imageElement.alt = 'Copy';
+        imageElement.title = 'Copy';
+        buttonElement.appendChild(imageElement);
+        const spanElement = document.createElement('span');
+        spanElement.innerText = tag.getAttribute('data-lang');
+        spanElement.classList.add('lang');
+        const wrapperDiv = document.createElement('div');
+        wrapperDiv.classList.add('wrapperContainer');
+        wrapperDiv.appendChild(spanElement);
+        wrapperDiv.appendChild(buttonElement);
+        tag.parentElement.appendChild(wrapperDiv);
+    });
+    /* To highlight code snippets */
     document.querySelectorAll("pre code").forEach(block => {
         hljs.highlightBlock(block as HTMLElement);
     })
