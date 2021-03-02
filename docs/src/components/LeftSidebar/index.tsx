59a6e22a54d31363450f6db5fb6fe9300c72004f
Harsh <harsh.rathi@thoughtspot.com>
2021-03-02 08:23:25 +0530

    - Fixed styling for nested ul

--- docs/src/components/LeftSidebar/index.tsx
+++ docs/src/components/LeftSidebar/index.tsx
@@ -8,17 +8,15 @@ const LeftSideBar = (props: {
     backLink: string;
     location: Location;
 }) => {
-
     const [pageid] = useState(props.location.search.slice(1).split('&')[0]);
     const [navContent, setNavContent] = useState('');
-    
+
     useEffect(() => {
         const divElement = document.createElement('div');
         divElement.innerHTML = props.navContent;
         const tag = divElement.querySelector(`a[href='?${pageid}']`);
         if (tag) {
             tag.classList.add('active');
-            tag.parentElement.classList.add('activelink');
         }
         setNavContent(divElement.innerHTML);
     }, [pageid, props.navContent]);
@@ -26,12 +24,12 @@ const LeftSideBar = (props: {
     return (
         <aside>
             {props.backLink && (
-                <BackButton title='SpotDev Home' backLink={props.backLink} />
+                <BackButton title="SpotDev Home" backLink={props.backLink} />
             )}
             <nav>
-                <h2 className='heading'>{props.navTitle}</h2>
+                <h2 className="heading">{props.navTitle}</h2>
                 <div
-                    className='navWrapper'
+                    className="navWrapper"
                     dangerouslySetInnerHTML={{
                         __html: navContent,
                     }}
