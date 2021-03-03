fdd2d428c6d757601f7f00a6ec9e3d70117f8a6b
harsh-ts <78337774+harsh-ts@users.noreply.github.com>
2021-03-03 16:58:00 +0530

    [SCAL-77544]: Add copy button on code blocks (#67)
    * - Added copy button and functionality
    
    * - Added tooltip when copy button is clicked
    
    * - Code refactoring
    
    * - Added code highlight and refactored code
    
    * - Code refactoring
    
    * - Added comments and cleaned up code
    
    * - Added seperate file for code highlight styling
    
    * - Added blank line at the end of file
    
    * - Added constants for colors used in highlight
    
    * - Resolved comments on PR
    
    * - Got rid of redundant stuff in svg file
    
    Co-authored-by: pankajp-ts <77004119+pankajp-ts@users.noreply.github.com>

--- docs/src/components/Document/index.tsx
+++ docs/src/components/Document/index.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './index.scss';
+import { customizeDocContent } from './helper';
 
-const Document = (props: { docTitle: string; docContent: string }) => (
-    <div className="documentWrapper">
-        <div
-            id={props.docTitle}
-            dangerouslySetInnerHTML={{
-                __html: props.docContent,
-            }}
-        />
-    </div>
-);
+const Document = (props: { docTitle: string; docContent: string }) => {
+
+    useEffect(() => {
+        customizeDocContent();
+    }, [props.docContent]);
+
+    return (
+        <div className="documentWrapper">
+            <div
+                id={props.docTitle}
+                dangerouslySetInnerHTML={{
+                    __html: props.docContent,
+                }}
+            />
+        </div>
+    );
+};
 
 export default Document;
