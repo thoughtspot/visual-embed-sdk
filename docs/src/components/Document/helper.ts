d0e4479938f59d205f15a5397d698bcf49b866fc
harsh-ts <78337774+harsh-ts@users.noreply.github.com>
2021-03-17 11:49:44 +0530

    [SCAL-80361] CSS fixes (#97)
    * - Added blue color for code highlight
    
    * - Added widthAuto class
    
    * - Added font family for text in code block
    
    * - Added new icon for copy button and fixed styling
    
    * - Added margin bottom for the definition list heading
    
    * - Added separate files for padding and margin classes
    
    * - Reduced space below the definition list heading

--- docs/src/components/Document/helper.ts
+++ docs/src/components/Document/helper.ts
@@ -1,7 +1,10 @@
 import hljs from 'highlight.js';
 import Copy from '../../assets/svg/copy.svg';
 
-export const enableCopyToClipboard = (element: HTMLElement, ...args: HTMLElement[]) => {
+export const enableCopyToClipboard = (
+    element: HTMLElement,
+    ...args: HTMLElement[]
+) => {
     element.addEventListener('click', () => {
         const textareaElement = document.createElement('textarea');
         textareaElement.value = (args[0] as HTMLElement).innerText;
@@ -20,31 +23,33 @@ export const enableCopyToClipboard = (element: HTMLElement, ...args: HTMLElement
         setTimeout(() => {
             element.parentElement.removeChild(divElement);
         }, 500);
-    })
+    });
 };
 
 export const customizeDocContent = () => {
     /* To get all the code blocks from document */
-    document.querySelectorAll('.listingblock>.content>.highlight>code').forEach((tag) => {
-        const buttonElement = document.createElement('button');
-        buttonElement.setAttribute('class', 'copyButton');
-        enableCopyToClipboard(buttonElement, tag as HTMLElement);
-        const imageElement = document.createElement('img');
-        imageElement.src = Copy;
-        imageElement.alt = 'Copy';
-        imageElement.title = 'Copy';
-        buttonElement.appendChild(imageElement);
-        const spanElement = document.createElement('span');
-        spanElement.innerText = tag.getAttribute('data-lang');
-        spanElement.classList.add('lang');
-        const wrapperDiv = document.createElement('div');
-        wrapperDiv.classList.add('wrapperContainer');
-        wrapperDiv.appendChild(spanElement);
-        wrapperDiv.appendChild(buttonElement);
-        tag.parentElement.appendChild(wrapperDiv);
-    });
+    document
+        .querySelectorAll('.listingblock>.content>.highlight>code')
+        .forEach((tag) => {
+            const buttonElement = document.createElement('button');
+            buttonElement.setAttribute('class', 'copyButton');
+            enableCopyToClipboard(buttonElement, tag as HTMLElement);
+            const imageElement = document.createElement('img');
+            imageElement.src = Copy;
+            imageElement.alt = 'Copy';
+            imageElement.title = 'Copy to clipboard';
+            buttonElement.appendChild(imageElement);
+            const spanElement = document.createElement('span');
+            spanElement.innerText = tag.getAttribute('data-lang');
+            spanElement.classList.add('lang');
+            const wrapperDiv = document.createElement('div');
+            wrapperDiv.classList.add('wrapperContainer');
+            wrapperDiv.appendChild(spanElement);
+            wrapperDiv.appendChild(buttonElement);
+            tag.parentElement.appendChild(wrapperDiv);
+        });
     /* To highlight code snippets */
-    document.querySelectorAll('pre code').forEach(block => {
+    document.querySelectorAll('pre code').forEach((block) => {
         hljs.highlightBlock(block as HTMLElement);
-    })
+    });
 };
