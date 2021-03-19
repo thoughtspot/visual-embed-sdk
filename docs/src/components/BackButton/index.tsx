f07786df994896c01f41f933631312376ebd62b5
Harsh <harsh.rathi@thoughtspot.com>
2021-03-19 12:50:14 +0530

    - Fixed styling for back button

--- docs/src/components/BackButton/index.tsx
+++ docs/src/components/BackButton/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import BackButtonImg from '../../assets/svg/backbtn.svg';
+import LeftArrow from '../../assets/svg/leftArrow.svg';
 import './index.scss';
 
 const BackButton = (props: { title: string; backLink: string }) => (
     <div className="backButtonWrapper">
         <button>
             <a href={props.backLink} target="_parent">
-                <img src={BackButtonImg} alt="BackButton" />
+                <img src={LeftArrow} alt="Left Arrow" />
             </a>
         </button>
         <p>{props.title}</p>
