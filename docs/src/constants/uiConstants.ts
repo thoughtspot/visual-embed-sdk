56744db57d61a0bca8c9af7e3d0cad403304c911
Harsh <harsh.rathi@thoughtspot.com>
2021-03-25 19:56:39 +0530

    - Added new constants

--- docs/src/constants/uiConstants.ts
+++ docs/src/constants/uiConstants.ts
@@ -1,8 +1,9 @@
-export const MAX_MOBILE_RESOLUTION: number = 1024;
+export const MAX_TABLET_RESOLUTION: number = 1024;
+export const MAX_MOBILE_RESOLUTION: number = 600;
 export const LEFT_NAV_WIDTH_DESKTOP: number = 310;
-export const LEFT_NAV_WIDTH_MOBILE: number = 260;
+export const LEFT_NAV_WIDTH_TABLET: number = 260;
 export const MIN_LEFT_NAV_WIDTH_DESKTOP: number = 150;
 export const MAX_LEFT_NAV_WIDTH_DESKTOP: number = 400;
-export const MIN_LEFT_NAV_WIDTH_MOBILE: number = 150;
-export const MAX_LEFT_NAV_WIDTH_MOBILE: number = 400;
+export const MIN_LEFT_NAV_WIDTH_TABLET: number = 150;
+export const MAX_LEFT_NAV_WIDTH_TABLET: number = 400;
 export const INTRO_WRAPPER_MARGIN_TOP: number = 81;
