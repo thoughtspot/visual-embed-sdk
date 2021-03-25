6aeeb558d146de15977e5afb7152e10d5c950585
Pankaj Pandey <pankaj.pandey@thoughtspot.com>
2021-03-25 11:19:58 +0530

    - preview in layground button url fixes testing

--- docs/src/configs/doc-configs.js
+++ docs/src/configs/doc-configs.js
@@ -3,8 +3,11 @@ module.exports = {
     DOC_NAV_PAGE_ID: 'nav',
     TS_HOST_PARAM: 'tshost',
     TS_ORIGIN_PARAM: 'origin',
+    TS_APP_ROOT_PARAM: 'appRoot',
     TS_PAGE_ID_PARAM: 'pageid',
     NAV_PREFIX: 'navprefix',
     NOT_FOUND_PAGE_ID: '404-error',
     TYPE_DOC_PREFIX: 'typedoc',
+    DEFAULT_HOST: 'https://try-everywhere.thoughtspot.cloud/v2',
+    DEFAULT_APP_ROOT: '/everywhere-standalone',
 };
