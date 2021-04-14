b2fcfe8f89c447ae5357b5fb59e908f8e8c0e1ac
harsh-ts <78337774+harsh-ts@users.noreply.github.com>
2021-04-14 18:52:30 +0530

    [SCAL-80348] Left nav collapse expand (#145)
    * - Added collpase and expand feature to Left Nav
    
    * - Made displayNone class globally accessible
    
    * - Added collapse and expand left nav feature to mobile display
    
    * - Added constant for image width
    
    * - Closed all left nav headings by default and added comments
    
    * - Added new lines in svg files
    
    * - Fixed arrow-down svg file
    
    * - Refactored code
    
    * - Added type def file for svg
    
    * - Resolved flicker issue
    
    * - Fixed icon styling for left nav
    
    * - Resolved commnets for PR
    
    * - Resolved comments for PR
    
    * - Added Navcontent dependency in useEffect
    
    * - Removed setTimeout
    
    * - Updated packages
    
    * - Updated package-lock.json

--- docs/src/components/LeftSidebar/index.tsx
+++ docs/src/components/LeftSidebar/index.tsx
@@ -13,6 +13,7 @@ import {
     MIN_LEFT_NAV_WIDTH_TABLET,
     MAX_MOBILE_RESOLUTION,
 } from '../../constants/uiConstants';
+import { collapseAndExpandLeftNav,addExpandCollapseImages } from './helper';
 import ClearIcon from '../../assets/svg/clear.svg';
 import MenuIcon from '../../assets/svg/menu.svg';
 import NavContent from './NavContent';
@@ -33,6 +34,9 @@ const LeftSideBar = (props: {
     const [navContent, setNavContent] = useState('');
     const { width, ref, height } = useResizeDetector();
 
+    const isMaxTabletResolution = !(props.docWidth < MAX_TABLET_RESOLUTION);
+    const isMaxMobileResolution = !(props.docWidth < MAX_MOBILE_RESOLUTION);
+
     useEffect(() => {
         const divElement = document.createElement('div');
         divElement.innerHTML = props.navContent;
@@ -42,20 +46,22 @@ const LeftSideBar = (props: {
         if (tag) {
             tag.classList.add('active');
         }
-        setNavContent(divElement.innerHTML);
-    }, [params, props.navContent]);
+        const updatedHTML = addExpandCollapseImages(divElement.innerHTML, params[TS_PAGE_ID_PARAM]);
+        setNavContent(updatedHTML);
+    }, [params[NAV_PREFIX], params[TS_PAGE_ID_PARAM], props.navContent]);
 
     useEffect(() => {
         props.handleLeftNavChange(width);
     }, [width]);
 
+    useEffect(() => {
+        collapseAndExpandLeftNav(ref.current as HTMLDivElement, props.setLeftNavOpen);
+    }, [params[TS_PAGE_ID_PARAM], isMaxMobileResolution, navContent]);
+
     const onMenuClick = () => {
         props.setLeftNavOpen(!props.leftNavOpen);
         document.documentElement.scrollTop = 0;
     };
-
-    const isMaxTabletResolution = !(props.docWidth < MAX_TABLET_RESOLUTION);
-    const isMaxMobileResolution = !(props.docWidth < MAX_MOBILE_RESOLUTION);
 
     const renderLeftNav = () => {
         return isMaxMobileResolution ? (
---
+++ docs/src/constants/selectorsContant.ts
@@ -0,0 +1,7 @@
+const selectors = {
+    linksContainer: '.navWrapper>.ulist>ul>li>div.ulist',
+    headings: '.navWrapper>.ulist>ul>li>p',
+    links: '.navWrapper>.ulist>ul>li>div.ulist>ul>li p>a'
+};
+
+export default selectors;
