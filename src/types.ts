539bd40f7ef519fe6b7f41ced542a70a9395f40e
Cristian Cojocaru <cristian.cojocaru@thoughtspot.com>
2021-07-15 17:53:38 +0300

    [SCAL-83054] Add query details button type to Action

--- src/types.ts
+++ src/types.ts
@@ -417,6 +417,7 @@ export enum Action {
     Separator = 'context-menu-item-separator',
     DrillDown = 'DRILL',
     RequestAccess = 'requestAccess',
+    QueryDetailsButtons = 'queryDetailsButtons',
 }
 
 export interface SessionInterface {
