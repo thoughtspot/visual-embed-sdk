a3e5e6a5c26c878214f0aa5554bd8cb21fc07881
Harsh <harsh.rathi@thoughtspot.com>
2021-08-03 10:18:10 +0530

    - Updated coverage threshold

--- jest.config.sdk.js
+++ jest.config.sdk.js
@@ -8,10 +8,10 @@ module.exports = {
     coveragePathIgnorePatterns: ['/node_modules/', '/test/'],
     coverageThreshold: {
         './src/': {
-            branches: 80,
-            functions: 80,
-            lines: 80,
-            statements: -10,
+            branches: 85,
+            functions: 85,
+            lines: 85,
+            statements: 85,
         },
     },
     testPathIgnorePatterns: ['/lib/', '/docs/'],
