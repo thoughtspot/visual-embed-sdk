6e4d7a6203a3fd0744d90a9f3a6959f5b0e001fb
Harsh <harsh.rathi@thoughtspot.com>
2021-08-03 10:20:17 +0530

    - Removed the statement coverage

--- jest.config.sdk.js
+++ jest.config.sdk.js
@@ -11,7 +11,6 @@ module.exports = {
             branches: 85,
             functions: 85,
             lines: 85,
-            statements: 85,
         },
     },
     testPathIgnorePatterns: ['/lib/', '/docs/'],
