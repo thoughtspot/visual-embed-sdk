4920e20e28bb28864399e305b02106a285de9604
Harsh <harsh.rathi@thoughtspot.com>
2021-08-05 11:57:27 +0530

    - Added breadcrums depends on routes change

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -3,7 +3,7 @@ import { useStaticQuery, graphql, navigate } from 'gatsby';
 import { useResizeDetector } from 'react-resize-detector';
 import { useFlexSearch } from 'react-use-flexsearch';
 import { queryStringParser, isPublicSite } from '../utils/app-utils';
-import passThroughHandler from '../utils/doc-utils';
+import { passThroughHandler, fetchChild } from '../utils/doc-utils';
 import Header from '../components/Header';
 import LeftSidebar from '../components/LeftSidebar';
 import Docmap from '../components/Docmap';
@@ -48,6 +48,7 @@ const IndexPage = ({ location }) => {
     const [docContent, setDocContent] = useState('');
     const [navTitle, setNavTitle] = useState('');
     const [navContent, setNavContent] = useState('');
+    const [breadcrumsData, setBreadcrumsData] = useState([]);
     const [backLink, setBackLink] = useState('');
     const [allPageIds, setAllPageIds] = useState([]);
     const [leftNavWidth, setLeftNavWidth] = useState(
@@ -123,9 +124,11 @@ const IndexPage = ({ location }) => {
             setNavTitle(edges[navIndex].node.pageAttributes.title);
 
             // get & set left navigation area content with dynamic link creation
-            setNavContent(
-                passThroughHandler(edges[navIndex].node.html, params),
-            );
+            const navContentData = passThroughHandler(edges[navIndex].node.html, params)
+            setNavContent(navContentData);
+
+            // set breadcrums data
+            setBreadcrumsData(fetchChild(navContentData));
 
             // get & set left navigation 'SpotDev Home' button url
             setBackLink(params[TS_ORIGIN_PARAM]);
@@ -143,7 +146,11 @@ const IndexPage = ({ location }) => {
         setNavTitle(edges[navIndex].node.pageAttributes.title);
 
         // get & set left navigation area content with dynamic link creation
-        setNavContent(passThroughHandler(edges[navIndex].node.html, params));
+        const navContentData = passThroughHandler(edges[navIndex].node.html, params)
+        setNavContent(navContentData);
+
+        // set breadcrums data
+        setBreadcrumsData(fetchChild(navContentData));
 
         // get & set left navigation 'Back' button url
         setBackLink(params[TS_ORIGIN_PARAM]);
@@ -227,6 +234,7 @@ const IndexPage = ({ location }) => {
         return '100%';
     };
 
+
     return (
         <div id="wrapper" data-theme={isDarkMode ? 'dark' : 'light'}>
             {isPublicSiteOpen && <Header location={location} />}
@@ -274,10 +282,13 @@ const IndexPage = ({ location }) => {
                         isDarkMode={isDarkMode}
                         isPublicSiteOpen={isPublicSiteOpen}
                     />
+
                     <div className="introWrapper">
                         <Document
+                            pageid={params[TS_PAGE_ID_PARAM]}
                             docTitle={docTitle}
                             docContent={docContent}
+                            breadcrumsData={breadcrumsData}
                             isPublicSiteOpen={isPublicSiteOpen}
                         />
                         <div>
