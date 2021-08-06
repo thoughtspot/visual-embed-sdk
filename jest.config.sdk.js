cde259dbf5f695d12cb5c854c42a3e3aed2b9b82
harsh-ts <78337774+harsh-ts@users.noreply.github.com>
2021-08-06 12:30:43 +0530

    [SCAL-90574]-Added coverageThreshold for /src folder (#262)
    * Added coverageThreshold for /src folder
    
    * SCAL-90617 - Moved gatsby to devdependecies to speed up npmi (#264)
    
    * review edits and fixes (#267)
    
    * typo-fixes (#268)
    
    * - Added docs in header to redirect it to home page (#270)
    
    * Changed July instances to August (#273)
    
    * SCAL-92354 add SDK changes to get iframe Center (#271)
    
    * SCAL-92354 add SDK changes to get iframe Center
    
    * iterate through comments
    
    * remove getIframeScrolled function
    
    * update executeCallback
    
    * update types
    
    * re-iterate through comments
    
    * lint errors
    
    * fix relative pos (#276)
    
    * fix relative pos
    
    * fix type error
    
    * modify getOffset
    
    * bump-sdk (#277)
    
    * [SCAL-91878] - Managed footer css to seperate it out from main content (#265)
    
    * - Managed footer css to seperate it out from main content
    
    * - Update index.scss
    
    * Added coverageThreshold for /src folder
    
    * - Updated coverage threshold
    
    * - Removed the statement coverage
    
    Co-authored-by: jbhanu-thoughtspot <53254394+jbhanu-thoughtspot@users.noreply.github.com>
    Co-authored-by: ShashiSubramanya <76986173+ShashiSubramanya@users.noreply.github.com>
    Co-authored-by: kailash-choudhary-thoughtspot <79596697+kailash-choudhary-thoughtspot@users.noreply.github.com>

--- jest.config.sdk.js
+++ jest.config.sdk.js
@@ -6,6 +6,13 @@ module.exports = {
     coverageDirectory: 'coverage/sdk/',
     coverageReporters: ['lcov', 'text', 'cobertura'],
     coveragePathIgnorePatterns: ['/node_modules/', '/test/'],
+    coverageThreshold: {
+        './src/': {
+            branches: 85,
+            functions: 85,
+            lines: 85,
+        },
+    },
     testPathIgnorePatterns: ['/lib/', '/docs/'],
     testMatch: ['**/src/**/*.spec.(ts|tsx)'],
     setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
