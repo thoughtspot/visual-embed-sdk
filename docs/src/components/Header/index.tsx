9d5a3117ccc3734f60edc88966d183f019486405
harsh-ts <78337774+harsh-ts@users.noreply.github.com>
2021-08-17 16:50:46 +0530

    - Disabled version dropdown (#322)
    * - Disabled version dropdown
    
    * - Commented the code
    
    * - Commented the Dropdown code

--- docs/src/components/Header/index.tsx
+++ docs/src/components/Header/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import TSLogo from '../../assets/svg/ts-logo-white.svg';
 import t from '../../utils/lang-utils';
-import Dropdown from '../Dropdown';
+// import Dropdown from '../Dropdown';
 import './index.scss';
 
 const Header = (props: { location: Location }) => {
@@ -45,7 +45,7 @@ const Header = (props: { location: Location }) => {
 
                         </div>
                     </div>
-                    <Dropdown location={props.location} />
+                    {/*  <Dropdown location={props.location} /> */}
                 </div>
             </section>
         </header>
