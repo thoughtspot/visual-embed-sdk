07da83e6de4d8685831979e4854b0372c1457056
ShashiSubramanya <76986173+ShashiSubramanya@users.noreply.github.com>
2021-09-15 16:14:40 +0530

    Update search.ts

--- src/embed/search.ts
+++ src/embed/search.ts
@@ -55,7 +55,7 @@ export interface SearchViewConfig extends ViewConfig {
     enableSearchAssist?: boolean;
     /**
      * If set to true, the tabular view is set as 
-     * the default format for presenting search data.
+     * the default format for presenting search data
      */
     forceTable?: boolean;
     /**
