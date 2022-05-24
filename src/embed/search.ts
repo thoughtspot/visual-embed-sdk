c6f63b07844517c43e9c2af42a53fe91acd10623
ShashiSubramanya <76986173+ShashiSubramanya@users.noreply.github.com>
2022-05-24 10:05:53 +0530

    version added for enableSearchAssist

--- src/embed/search.ts
+++ src/embed/search.ts
@@ -51,6 +51,7 @@ export interface SearchViewConfig extends ViewConfig {
     hideResults?: boolean;
     /**
      * If set to true, the Search Assist feature is enabled.
+     * @version SDK: 1.14.0 | ThoughtSpot: 8.6.0.cl
      */
     enableSearchAssist?: boolean;
     /**
