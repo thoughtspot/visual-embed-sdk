a26ccc53330760fa141ad163c6da979b23d266b6
Akash Mohapatra <akash.mohapatra@thoughtspot.com>
2022-06-01 00:46:23 +0530

    Replaced the copy button with an anchor element that navigates the stadalone doc website to the correct link or publishes an event on the embed dev portal to update the query param therein

--- docs/src/components/LinkableHeader/index.test.tsx
+++ docs/src/components/LinkableHeader/index.test.tsx
@@ -1,17 +1,10 @@
 import React from 'react'
-import { render, fireEvent, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import LinkableHeader from './index'
 
 describe('Linkable Header', () => {
     it('Should render the right content', () => {
         const { container } = render(<LinkableHeader tag="h4" id="test-id">Test Header Content</LinkableHeader>);
         expect(container).toMatchSnapshot();
-        fireEvent.click(screen.getByText('🔗'));
     })
-    it('Should should clean up itself after the click', () => {
-        const { container } = render(<LinkableHeader tag="h4" id="test-id">Test Header Content</LinkableHeader>);
-        fireEvent.click(screen.getByText('🔗'));
-        expect(container.querySelectorAll('#copy-link-text').length == 0);
-    })
-
 })
--- docs/src/components/LinkableHeader/index.tsx
+++ docs/src/components/LinkableHeader/index.tsx
@@ -8,24 +8,11 @@ type LinkableSectionProps = {
 };
 
 const LinkableHeader = (props: LinkableSectionProps) => {
-    function copyLink(event) {
-        const link = new URL(location.href);
-        link.hash = event.target.dataset?.hash ?? '';
-        const textareaElement = document.createElement('textarea');
-        textareaElement.id = "copy-link-text";
-        textareaElement.value = link.href;
-        document.body.appendChild(textareaElement);
-        textareaElement.select();
-        document.execCommand('copy');
-        document.body.removeChild(textareaElement);
-    }
-
     const Tag = props.tag as keyof JSX.IntrinsicElements;
-
     return (
-        <Tag id={props.id} className="copy-link-header">
+        <Tag id={props.id} className="linkable-header">
             {props.children}
-            <button onClick={copyLink} title="Copy link" data-hash={props.id} className="copy-link-button">🔗</button>
+            <a href={`#${props.id}`} className="link-element">🔗</a>
         </Tag>
     );
 };
