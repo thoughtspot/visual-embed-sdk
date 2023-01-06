23e26f3e970ce87c6cb8312b1aab438e772db4f1
ShashiSubramanya <>
2023-01-06 16:07:35 +0530

    typedoc refresh

--- src/types.ts
+++ src/types.ts
@@ -623,7 +623,7 @@ export enum EmbedEvent {
      */
     DialogClose = 'dialog-close',
     /**
-     * Emitted when the Liveboard shell loads. 
+     * Emitted when the Liveboard shell loads.
      * You can use this event as a hook to trigger
      * other events on the rendered Liveboard.
      * @version SDK: 1.9.1 | ThoughtSpot: 8.1.0.cl, 8.4.1-sw
