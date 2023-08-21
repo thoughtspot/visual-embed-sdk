2d87dab4092d92bc6e4b75dc25f042e746f313f8
Aditya Mittal <114516106+adityamittal3107@users.noreply.github.com>
2023-08-21 15:40:20 +0530

    update help text of TML global methods (#945)
    * Update help text
    
    * Update help text of global methods

--- src/embed/base.ts
+++ src/embed/base.ts
@@ -257,6 +257,7 @@ export const renderInQueue = (fn: (next?: (val?: any) => void) => Promise<any>):
 };
 
 /**
+ * Imports TML representation of the metadata objects into ThoughtSpot.
  * @param data
  * @version SDK: 1.23.0 | ThoughtSpot: 9.4.0.cl
  */
@@ -304,6 +305,7 @@ export const executeTML = async (data: executeTMLInput): Promise<any> => {
 };
 
 /**
+ * Exports TML representation of the metadata objects from ThoughtSpot in JSON or YAML format.
  * @param data
  * @version SDK: 1.23.0 | ThoughtSpot: 9.4.0.cl
  */
