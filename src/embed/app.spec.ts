50d36586c083917effd1f0dd6358005ea2a840b0
deepakkumar-thoughtspot <143430974+deepakkumar-thoughtspot@users.noreply.github.com>
2023-09-07 12:47:19 +0530

    Update app.spec.ts
    changed flag name.

--- src/embed/app.spec.ts
+++ src/embed/app.spec.ts
@@ -284,17 +284,17 @@ describe('App embed tests', () => {
         });
     });
 
-    test('Should add showHomepageLeftNav flag to the iframe src', async () => {
+    test('Should add hideHomepageLeftNav flag to the iframe src', async () => {
         const appEmbed = new AppEmbed(getRootEl(), {
             ...defaultViewConfig,
-            showHomepageLeftNav: true,
+            hideHomepageLeftNav: false,
         } as AppViewConfig);
 
         appEmbed.render();
         await executeAfterWait(() => {
             expectUrlMatchesWithParams(
                 getIFrameSrc(),
-                `http://${thoughtSpotHost}/?embedApp=true&primaryNavHidden=true&profileAndHelpInNavBarHidden=false&modularHomeExperience=false&showHomepageLeftNav=true${defaultParams}${defaultParamsPost}#/home`,
+                `http://${thoughtSpotHost}/?embedApp=true&primaryNavHidden=true&profileAndHelpInNavBarHidden=false&modularHomeExperience=false&hideHomepageLeftNav=false${defaultParams}${defaultParamsPost}#/home`,
             );
         });
     });
