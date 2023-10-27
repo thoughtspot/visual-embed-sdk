4487d4af96f00699fa6652dcc2921e34754f1f5c
sastaachar <justinjohnmathew100@gmail.com>
2023-10-27 18:06:53 +0530

    added test to increase coverage

--- src/utils.spec.ts
+++ src/utils.spec.ts
@@ -11,6 +11,7 @@ import {
     getRedirectUrl,
     checkReleaseVersionInBeta,
     getRuntimeParameters,
+    removeTypename,
 } from './utils';
 import { RuntimeFilterOp } from './types';
 
@@ -178,5 +179,23 @@ describe('unit test for utils', () => {
 
     test('when suppressBetaWarning is false ReleaseVersion is 7.0.1', () => {
         expect(checkReleaseVersionInBeta('7.0.1', false)).toBe(true);
+    });
+    test('removeTypename should removed __typename', () => {
+        const input = {
+            test: 'test',
+            __typename: 'should be removed',
+            obj: {
+                test: 'test',
+                __typename: 'should be removed',
+            },
+        };
+        const result = removeTypename(input);
+        const expectedResult = {
+            test: 'test',
+            obj: {
+                test: 'test',
+            },
+        };
+        expect(result).toEqual(expectedResult);
     });
 });
