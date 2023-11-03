1317a65390bbd62a7e4d8468ad8d67912f899ce3
Ashish Shubham <ashubham@gmail.com>
2023-11-03 10:02:05 -0700

    SCAL-173330 GetAnswerSession Host Event

--- src/utils/graphql/sourceService.spec.ts
+++ src/utils/graphql/sourceService.spec.ts
@@ -3,6 +3,15 @@ import { getSourceDetail } from './sourceService';
 
 describe('Source service tests', () => {
     test('Should return source detail and cache it', async () => {
+        fetchMock.mockResponseOnce(JSON.stringify({
+            data: {
+                getSourceDetail: [{
+                    Bla: {
+                        id: {},
+                    },
+                }],
+            },
+        }));
         await getSourceDetail('https://tshost', 'id');
         await getSourceDetail('https://tshost', 'id');
         expect(fetchMock).toBeCalledTimes(1);
