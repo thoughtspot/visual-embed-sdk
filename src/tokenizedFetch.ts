9e5a8f5fca0eb97a27225e25cfc00a3b69af2321
Ashish Shubham <ashish.shubham@thoughtspot.com>
2024-03-27 12:00:28 +0530

    Exposed tokenized Fetch

--- src/tokenizedFetch.ts
+++ src/tokenizedFetch.ts
@@ -3,6 +3,13 @@ import { getEmbedConfig } from './embed/embedConfig';
 
 import { AuthType } from './types';
 
+/**
+ * Fetch wrapper that adds the authentication token to the request.
+ * Use this to call the ThoughtSpot APIs when using the visual embed sdk.
+ *
+ * @param input
+ * @param init
+ */
 export const tokenizedFetch: typeof fetch = async (input, init): Promise<Response> => {
     const embedConfig = getEmbedConfig();
     if (embedConfig.authType !== AuthType.TrustedAuthTokenCookieless) {
