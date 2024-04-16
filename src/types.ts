174c38c043b0a1d108c3ff41402184f002f4cf1d
Bishal KR <bishal.kr@thoughtspot.com>
2024-04-16 18:38:50 +0530

    Added verison

--- src/types.ts
+++ src/types.ts
@@ -3281,6 +3281,7 @@ export enum Action {
      * ```js
      * disabledActions: [Action.AddColumnSet]
      * ```
+     * @version SDK: 1.29.0 | Thoughtspot: 10.0.0.cl
      */
     AddColumnSet = 'addSimpleCohort',
     /**
@@ -3291,6 +3292,7 @@ export enum Action {
      * ```js
      * disabledActions: [Action.AddQuerySet]
      * ```
+     * @version SDK: 1.29.0 | Thoughtspot: 10.0.0.cl
      */
     AddQuerySet = 'addAdvancedCohort',
     /**
