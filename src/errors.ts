e3d3d99abccca7c71993a385695a4e0abb9867a0
Prashant Patil <141843298+yinstardev@users.noreply.github.com>
2024-05-24 14:59:39 +0530

    SCAL-196825 Error Banner Playground for Empty LB value (#1125)

--- src/errors.ts
+++ src/errors.ts
@@ -1,6 +1,6 @@
 export const ERROR_MESSAGE = {
     INVALID_THOUGHTSPOT_HOST: 'Error parsing ThoughtSpot host. Please provide a valid URL.',
-    LIVEBOARD_VIZ_ID_VALIDATION: 'Please provide either liveboardId or pinboardId',
+    LIVEBOARD_VIZ_ID_VALIDATION: 'Please select a liveboard to embed.',
     TRIGGER_TIMED_OUT: 'Trigger timedout in getting response',
     SEARCHEMBED_BETA_WRANING_MESSAGE: 'Search Embed is in Beta in this release.',
     SAGE_EMBED_BETA_WARNING_MESSAGE: 'Sage Embed is in Beta in this release.',
