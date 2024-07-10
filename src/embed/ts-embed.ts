76005de363dd6618fb5f88d4485428c8a33b2520
Ashish Shubham <ashish.shubham@thoughtspot.com>
2024-07-10 13:53:46 -0700

    remove stray console logs

--- src/embed/ts-embed.ts
+++ src/embed/ts-embed.ts
@@ -604,7 +604,6 @@ export class TsEmbed {
      * @param url - The URL of the embedded ThoughtSpot app.
      */
     protected async renderIFrame(url: string): Promise<any> {
-        console.log('here 7');
         if (this.isError) {
             return null;
         }
@@ -614,10 +613,8 @@ export class TsEmbed {
         if (url.length > URL_MAX_LENGTH) {
             // warn: The URL is too long
         }
-        console.log('here 8');
 
         return renderInQueue((nextInQueue) => {
-            console.log('here 6');
             const initTimestamp = Date.now();
 
             this.executeCallbacks(EmbedEvent.Init, {
@@ -630,7 +627,6 @@ export class TsEmbed {
             uploadMixpanelEvent(MIXPANEL_EVENT.VISUAL_SDK_RENDER_START);
             return getAuthPromise()
                 ?.then((isLoggedIn: boolean) => {
-                    console.log('here 9', isLoggedIn);
                     if (!isLoggedIn) {
                         this.handleInsertionIntoDOM(this.embedConfig.loginFailedMessage);
                         return;
@@ -655,9 +651,7 @@ export class TsEmbed {
                     this.iFrame.addEventListener('error', () => {
                         nextInQueue();
                     });
-                    console.log('here 10', this.iFrame);
                     this.handleInsertionIntoDOM(this.iFrame);
-                    console.log('here 11', document.body.innerHTML);
                     const prefetchIframe = document.querySelectorAll('.prefetchIframe');
                     if (prefetchIframe.length) {
                         prefetchIframe.forEach((el) => {
