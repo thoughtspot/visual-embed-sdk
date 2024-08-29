084e7790048dae09a3f6bca0b623fd7e78f652bd
Priyanshu Kumar <priyanshu.kumar@thoughtspot.com>
2024-08-29 22:26:48 +0530

    SCAL-207429 added test cases to increase coverage

---
+++ src/utils/graphql/graphql-request.spec.ts
@@ -0,0 +1,38 @@
+import 'jest-fetch-mock';
+import { getSourceDetail } from './sourceService';
+import * as tokenizedFetchUtil from '../../tokenizedFetch';
+import { graphqlQuery } from './graphql-request';
+
+const getSourceDetailQuery = `
+    query GetSourceDetail($ids: [GUID!]!) {
+        getSourceDetailById(ids: $ids, type: LOGICAL_TABLE) {
+            id
+            name
+        }
+    }  
+`;
+
+const thoughtSpotHost = 'TSHOST';
+
+describe('graphQl tests', () => {
+    test('should call tokenizedFetch with correct parameters when graphqlQuery is called', async () => {
+        jest.spyOn(tokenizedFetchUtil, 'tokenizedFetch');
+
+        const details = await graphqlQuery({
+            query: getSourceDetailQuery,
+            variables: {
+                ids: [2],
+            },
+            thoughtSpotHost,
+        });
+
+        expect(tokenizedFetchUtil.tokenizedFetch).toBeCalledWith('TSHOST/prism/?op=GetSourceDetail', {
+            body: '{"operationName":"GetSourceDetail","query":"\\n    query GetSourceDetail($ids: [GUID!]!) {\\n        getSourceDetailById(ids: $ids, type: LOGICAL_TABLE) {\\n            id\\n            name\\n        }\\n    }  \\n","variables":{"ids":[2]}}',
+            credentials: 'include',
+            headers: {
+                accept: '*/*', 'accept-language': 'en-us', 'content-type': 'application/json;charset=UTF-8', 'x-requested-by': 'ThoughtSpot',
+            },
+            method: 'POST',
+        });
+    });
+});
