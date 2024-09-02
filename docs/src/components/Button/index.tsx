7c55547baa8094ae40fc30c4f9122f9dd5d52a8c
Justin Mathew <42416647+sastaachar@users.noreply.github.com>
2024-09-02 14:20:22 +0530

    SCAL-221690 : Remove old docs code (#3)

--- docs/src/components/Button/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './index.scss';
-
-const Button = (props: { label: string; type: string }) => (
-    <button data-testid="btn" className={`button ${props.type}`}>{props.label}</button>
-);
-
-export default Button;
--- docs/src/pages/404.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Page404 from '../components/Page404';
-
-export default function NotFoundPage() {
-    return <Page404 />;
-}
