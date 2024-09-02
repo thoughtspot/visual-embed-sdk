5edd843fd11324742fc1e4aef52368d551cd3edf
sastaachar <justinjohnmathew100@gmail.com>
2024-09-02 17:40:21 +0530

    1.32.2-release : Update

--- scripts/is-publish-allowed.js
+++ scripts/is-publish-allowed.js
@@ -1,8 +1,8 @@
 const currentGitBranch = require('current-git-branch');
 
 const tag = process.env.npm_config_tag || 'latest';
-if(tag === 'latest' && currentGitBranch() !== 'release') {
-    console.error("Can only do prereleases from branches other than release");
+if(tag === 'latest' && currentGitBranch() !== 'main') {
+    console.error("Can only do prereleases from branches other than main");
     console.log("Use 'npm publish --tag <tag>' to create a prerelease.")
     process.exit(1);
 };
