85612fd1cf7937678c49bface0596cd3e0d4958e
Yin Yue <prinostaged@gmail.com>
2024-10-24 02:00:56 +0530

    SCAL-229730-test ut-lint max-line-length

--- src/embed/conversation.spec.ts
+++ src/embed/conversation.spec.ts
@@ -55,6 +55,8 @@ describe('ConversationEmbed', () => {
         const conversationEmbed = new ConversationEmbed(getRootEl(), viewConfig);
         (conversationEmbed as any).handleError = jest.fn();
         await conversationEmbed.render();
-        expect((conversationEmbed as any).handleError).toHaveBeenCalledWith(ERROR_MESSAGE.SPOTTER_EMBED_WORKSHEED_ID_NOT_FOUND);
+        expect((conversationEmbed as any).handleError).toHaveBeenCalledWith(
+            ERROR_MESSAGE.SPOTTER_EMBED_WORKSHEED_ID_NOT_FOUND
+        );
     });
 });
