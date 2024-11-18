41e8fd07a0e10de230ede34f64eb4ac6b6d3a26e
Yin Yue <prinostaged@gmail.com>
2024-11-18 22:09:14 +0530

    native support remove window1

--- src/embed/base.ts
+++ src/embed/base.ts
@@ -192,7 +192,8 @@ function backwardCompat(embedConfig: EmbedConfig): EmbedConfig {
  * @group Authentication / Init
  */
 export const init = (embedConfig: EmbedConfig): AuthEventEmitter => {
-    const isReactNative = typeof navigator !== 'undefined' && navigator.product === 'ReactNative';
+    // const isReactNative = typeof navigator !== 'undefined' && navigator.product === 'ReactNative';
+    const isReactNative = true;
     console.log('is it reac native env? : ', isReactNative);
     sanity(embedConfig);
     resetCachedAuthToken();
