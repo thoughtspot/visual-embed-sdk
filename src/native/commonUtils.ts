d98cfaac2d11ded3e13c88c9ff2e0ea2bc91ff33
Yin Yue <prinostaged@gmail.com>
2024-12-02 22:19:58 +0530

    get lb url a1

--- src/native/commonUtils.ts
+++ src/native/commonUtils.ts
@@ -7,6 +7,7 @@ const { version } = pkgInfo;
 interface WebViewConfig {
   host: string;
   authType: AuthType;
+  liveboardId: string;
 }
 
 /**
@@ -15,24 +16,26 @@ interface WebViewConfig {
  * @returns {string} The constructed WebView URL.
  */
 export const getWebViewUrl = (wConfig: WebViewConfig): string => {
-    const hostAppUrl = encodeURIComponent(
-        wConfig.host.includes('localhost')
-      || wConfig.host.includes('127.0.0.1')
-      || wConfig.host.includes('10.0.2.2')
-            ? 'local-host'
-            : wConfig.host,
-    );
+  const hostAppUrl = encodeURIComponent(
+      wConfig.host.includes('localhost')
+    || wConfig.host.includes('127.0.0.1')
+    || wConfig.host.includes('10.0.2.2')
+          ? 'local-host'
+          : wConfig.host,
+  );
 
-    const queryParams = {
-        [Param.EmbedApp]: true,
-        [Param.HostAppUrl]: hostAppUrl,
-        [Param.Version]: version,
-        [Param.AuthType]: wConfig.authType,
-        ...(wConfig.authType === AuthType.TrustedAuthTokenCookieless
-            ? { [Param.cookieless]: true }
-            : {}),
-    };
+  const queryParams = {
+      [Param.EmbedApp]: true,
+      [Param.HostAppUrl]: hostAppUrl,
+      [Param.Version]: version,
+      [Param.AuthType]: wConfig.authType,
+      [Param.livedBoardEmbed]: true,
+      ...(wConfig.authType === AuthType.TrustedAuthTokenCookieless
+          ? { [Param.cookieless]: true }
+          : {}),
+  };
 
-    const queryString = getQueryParamString(queryParams);
-    return `${wConfig.host}/embed?${queryString}`;
+  const queryString = getQueryParamString(queryParams);
+  const liveboardPath = `#/embed/viz/${encodeURIComponent(wConfig.liveboardId)}`;
+  return `${wConfig.host}/embed?${queryString}${liveboardPath}`;
 };
