fc4985122f1a3262307f8bf87bdc9d1e5cfd109e
Yin Yue <prinostaged@gmail.com>
2024-12-02 23:22:17 +0530

    get lb url a3

--- src/native/commonUtils.ts
+++ src/native/commonUtils.ts
@@ -18,39 +18,38 @@ interface WebViewConfig {
  * @returns The constructed WebView URL.
  */
 export const getWebViewUrl = async (config: WebViewConfig, webViewRef: any): Promise<string> => {
-  if (typeof config.getAuthToken !== 'function') {
-    throw new Error('`getAuthToken` must be a function that returns a Promise.');
-  }
+    if (typeof config.getAuthToken !== 'function') {
+        throw new Error('`getAuthToken` must be a function that returns a Promise.');
+    }
 
-  const authToken = await config.getAuthToken();
-  if (!authToken) {
-    throw new Error('Failed to fetch initial authentication token.');
-  }
+    const authToken = await config.getAuthToken();
+    if (!authToken) {
+        throw new Error('Failed to fetch initial authentication token.');
+    }
 
-  const hostAppUrl = encodeURIComponent(
-    config.host.includes('localhost')
+    const hostAppUrl = encodeURIComponent(
+        config.host.includes('localhost')
       || config.host.includes('127.0.0.1')
       || config.host.includes('10.0.2.2')
-      ? 'local-host'
-      : config.host
-  );
+            ? 'local-host'
+            : config.host,
+    );
 
-  const queryParams = {
-    [Param.EmbedApp]: true,
-    [Param.HostAppUrl]: hostAppUrl,
-    [Param.Version]: pkgInfo.version,
-    [Param.AuthType]: config.authType,
-    [Param.livedBoardEmbed]: true,
-  };
+    const queryParams = {
+        [Param.EmbedApp]: true,
+        [Param.HostAppUrl]: hostAppUrl,
+        [Param.Version]: pkgInfo.version,
+        [Param.AuthType]: config.authType,
+        [Param.livedBoardEmbed]: true,
+    };
 
-  const queryString = getQueryParamString(queryParams);
-  const webViewUrl = `${config.host}/embed?${queryString}#/embed/viz/${encodeURIComponent(config.liveboardId)}`;
+    const queryString = getQueryParamString(queryParams);
+    const webViewUrl = `${config.host}/embed?${queryString}#/embed/viz/${encodeURIComponent(config.liveboardId)}`;
 
-  setupWebViewMessageHandler(config, webViewRef);
+    setupWebViewMessageHandler(config, webViewRef);
 
-  return webViewUrl;
+    return webViewUrl;
 };
-
 
 /**
  * Sets up the message handling for the WebView to handle events like `appInit` and `AuthExpired`.
@@ -58,67 +57,67 @@ export const getWebViewUrl = async (config: WebViewConfig, webViewRef: any): Pro
  * @param webViewRef Reference to the WebView instance for communication.
  */
 const setupWebViewMessageHandler = (config: WebViewConfig, webViewRef: any) => {
-  const injectJavaScript = (code: string) => {
-    webViewRef.current?.injectJavaScript(code);
-  };
+    const injectJavaScript = (code: string) => {
+        webViewRef.current?.injectJavaScript(code);
+    };
 
-  const handleMessage = async (event: any) => {
-    const message = JSON.parse(event.nativeEvent.data);
+    const handleMessage = async (event: any) => {
+        const message = JSON.parse(event.nativeEvent.data);
 
-    switch (message.type) {
-      case 'appInit': {
-        try {
-          const authToken = await config.getAuthToken();
-          const initPayload = {
-            type: 'appInit',
-            data: {
-              host: config.host,
-              authToken,
-            },
-          };
-          injectJavaScript(`window.postMessage(${JSON.stringify(initPayload)}, '*');`);
-        } catch (error) {
-          console.error('Error handling appInit:', error);
+        switch (message.type) {
+            case 'appInit': {
+                try {
+                    const authToken = await config.getAuthToken();
+                    const initPayload = {
+                        type: 'appInit',
+                        data: {
+                            host: config.host,
+                            authToken,
+                        },
+                    };
+                    injectJavaScript(`window.postMessage(${JSON.stringify(initPayload)}, '*');`);
+                } catch (error) {
+                    console.error('Error handling appInit:', error);
+                }
+                break;
+            }
+
+            case 'ThoughtspotAuthExpired': {
+                try {
+                    const newAuthToken = await config.getAuthToken();
+                    if (newAuthToken) {
+                        const authExpirePayload = {
+                            type: 'ThoughtspotAuthExpired',
+                            data: { authToken: newAuthToken },
+                        };
+                        injectJavaScript(`window.postMessage(${JSON.stringify(authExpirePayload)}, '*');`);
+                    }
+                } catch (error) {
+                    console.error('Error refreshing token on expiry:', error);
+                }
+                break;
+            }
+
+            case 'ThoughtspotAuthFailure': {
+                try {
+                    const newAuthToken = await config.getAuthToken();
+                    if (newAuthToken) {
+                        const authFailurePayload = {
+                            type: 'ThoughtspotAuthFailure',
+                            data: { authToken: newAuthToken },
+                        };
+                        injectJavaScript(`window.postMessage(${JSON.stringify(authFailurePayload)}, '*');`);
+                    }
+                } catch (error) {
+                    console.error('Error refreshing token on failure:', error);
+                }
+                break;
+            }
+
+            default:
+                console.warn('Unhandled message type:', message.type);
         }
-        break;
-      }
+    };
 
-      case 'ThoughtspotAuthExpired': {
-        try {
-          const newAuthToken = await config.getAuthToken();
-          if (newAuthToken) {
-            const authExpirePayload = {
-              type: 'ThoughtspotAuthExpired',
-              data: { authToken: newAuthToken },
-            };
-            injectJavaScript(`window.postMessage(${JSON.stringify(authExpirePayload)}, '*');`);
-          }
-        } catch (error) {
-          console.error('Error refreshing token on expiry:', error);
-        }
-        break;
-      }
-
-      case 'ThoughtspotAuthFailure': {
-        try {
-          const newAuthToken = await config.getAuthToken();
-          if (newAuthToken) {
-            const authFailurePayload = {
-              type: 'ThoughtspotAuthFailure',
-              data: { authToken: newAuthToken },
-            };
-            injectJavaScript(`window.postMessage(${JSON.stringify(authFailurePayload)}, '*');`);
-          }
-        } catch (error) {
-          console.error('Error refreshing token on failure:', error);
-        }
-        break;
-      }
-
-      default:
-        console.warn('Unhandled message type:', message.type);
-    }
-  };
-
-  webViewRef.current?.addEventListener('message', handleMessage);
+    webViewRef.current?.addEventListener('message', handleMessage);
 };
