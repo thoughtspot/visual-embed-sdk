ebe400fde90f43586577a1f0d4caaced5ea1180e
bhanu.jupally <bhanu.jupally@thoughtspot.com>
2025-01-02 13:11:42 -0800

    SCAL-230365 - Added a min height to the iframe, so it doesn't go below it when fullheight scales down

--- src/embed/ts-embed.ts
+++ src/embed/ts-embed.ts
@@ -594,6 +594,8 @@ export class TsEmbed {
 
         iFrame.style.width = `${width}`;
         iFrame.style.height = `${height}`;
+        // Set minimum height to the frame so that, scaling down on the fullheight doesn't impact this.
+        iFrame.style.minHeight = `${height}`;
         iFrame.style.border = '0';
         iFrame.name = 'ThoughtSpot Embedded Analytics';
         return iFrame;
