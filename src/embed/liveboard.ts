a87860197f53b2f5ce2416f80fdcb0919c83f939
Jishnu Pavithran <jishnu.pavithran@thoughtspot.com>
2025-03-17 14:07:24 +0530

    lint fix

--- src/embed/liveboard.ts
+++ src/embed/liveboard.ts
@@ -472,9 +472,8 @@ export class LiveboardEmbed extends V1Embed {
             coverAndFilterOptionInPDF = true,
         } = this.viewConfig;
 
-        const preventLiveboardFilterRemoval =
-            this.viewConfig.preventLiveboardFilterRemoval ||
-            this.viewConfig.preventPinboardFilterRemoval;
+        const preventLiveboardFilterRemoval = this.viewConfig.preventLiveboardFilterRemoval
+        || this.viewConfig.preventPinboardFilterRemoval;
 
         if (fullHeight === true) {
             params[Param.fullHeight] = true;
@@ -642,9 +641,8 @@ export class LiveboardEmbed extends V1Embed {
 
         if (isUndefined(embedObj)) return;
 
-        const showDifferentLib =
-            this.viewConfig.liveboardId &&
-            embedObj.viewConfig.liveboardId !== this.viewConfig.liveboardId;
+        const showDifferentLib = this.viewConfig.liveboardId
+            && embedObj.viewConfig.liveboardId !== this.viewConfig.liveboardId;
 
         if (showDifferentLib) {
             const libId = this.viewConfig.liveboardId;
