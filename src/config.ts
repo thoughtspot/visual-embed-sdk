90496e9ace0475d6f8f138163b1c50fd447db002
Prashant.patil <prashant.patil@hyd-prashant.pa>
2025-03-18 18:12:07 +0530

    windo.locatin

--- src/config.ts
+++ src/config.ts
@@ -33,15 +33,13 @@ export const getThoughtSpotHost = (config: EmbedConfig): string => {
         throw new Error(ERROR_MESSAGE.INVALID_THOUGHTSPOT_HOST);
     }
 
-    const protocol = urlParts[2] || window.location.protocol;
+    const protocol = urlParts[2] || (typeof window !== 'undefined' ? window.location.protocol : 'https:');
     const host = urlParts[3];
     let path = urlParts[6];
     // Lose the trailing / if any
     if (path.charAt(path.length - 1) === '/') {
         path = path.substring(0, path.length - 1);
     }
-    // const urlParams = urlParts[7];
-    // const hash = urlParts[8];
     return `${protocol}//${host}${path}`;
 };
 
