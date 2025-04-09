3fe54b83c2968ca53fe7fd927f9a100a2c988dbe
rani2655 <147147811+rani2655@users.noreply.github.com>
2025-04-09 10:17:48 +0530

    documentation changes
    Co-authored-by: ShashiSubramanya <shashikala.subramanya@thoughtspot.com>
    Co-authored-by: ShashiSubramanya <76986173+ShashiSubramanya@users.noreply.github.com>

--- src/types.ts
+++ src/types.ts
@@ -626,7 +626,7 @@ export interface EmbedConfig {
      *     }
      * });
      * ```
-     *  @version SDK 1.37.0 | ThoughtSpot: 10.7.0.cl
+     *  @version SDK 1.37.0 | ThoughtSpot: 10.8.0.cl
      */
     customVariablesForThirdPartyTools?: Record< string, any >;
 
@@ -2312,16 +2312,19 @@ export enum EmbedEvent {
     /**
      * Emits when a table visualization is rendered in
      * the ThoughtSpot embedded app.
+     *
      * You can also use this event as a hook to trigger host events
      * such as `HostEvent.TransformTableVizData` on the table visualization.
      * The event payload contains the data used in the rendered table.
      * You can extract the relevant data from the payload
      * stored in `payload.data.data.columnDataLite`.
+     *
      * `columnDataLite` is a multidimensional array that contains
      * data values for each column, which was used in the query to
      * generate the table visualization. To find and modify specific cell data,
      * you can either loop through the array or directly access a cell if
      * you know its position and data index.
+     *
      * In the following code sample, the first cell in the first column
      * (`columnDataLite[0].dataValue[0]`) is set to `new fob`.
      * Note that any changes made to the data in the payload will only update the
@@ -2331,6 +2334,7 @@ export enum EmbedEvent {
      * payload in the `columnDataLite` is passed on to
      * `HostEvent.TransformTableVizData` and trigger an update to
      * the table visualization.
+     *
      * If the Row-Level Security (RLS) rules are applied on the
      * Worksheet or Model, exercise caution when changing column
      * or table cell values to maintain data security.
@@ -2345,7 +2349,7 @@ export enum EmbedEvent {
      *      searchEmbed.trigger(HostEvent.TransformTableVizData, columnDataLite);
      * })
      * ```
-     * @version SDK: 1.35.12 | ThoughtSpot: 10.7.0.cl
+     * @version SDK: 1.38.0 | ThoughtSpot: 10.8.0.cl
      */
      TableVizRendered = 'TableVizRendered',
      /**
@@ -2358,7 +2362,7 @@ export enum EmbedEvent {
      *     console.log('payload', payload);
      * })
      *```
-     * @version SDK : 1.36.0 | ThoughtSpot : 10.8.0.cl
+     * @version SDK : 1.38.0 | ThoughtSpot : 10.8.0.cl
      */
     CreateLiveboard = 'createLiveboard',
     /**
@@ -2807,7 +2811,7 @@ export enum HostEvent {
      * ```js
      * liveboardEmbed.trigger(HostEvent.Remove)
      * ```
-     * @version SDK: 1.37.0 | ThoughtSpot: 10.8.0.cl, 10.10.0.sw
+     * @version SDK: 1.38.0 | ThoughtSpot: 10.8.0.cl, 10.10.0.sw
      */
     Remove = 'delete',
     /**
@@ -3355,7 +3359,7 @@ export enum HostEvent {
      * ```js
      * liveboardEmbed.trigger(HostEvent.InfoSuccess, data);
      *```
-     * @version SDK: 1.36.0 | Thoughtspot: 10.6.0.cl
+     * @version SDK: 1.36.0 | ThoughtSpot: 10.6.0.cl
      */
      InfoSuccess = 'InfoSuccess',
     /**
@@ -3402,7 +3406,7 @@ export enum HostEvent {
      *      searchEmbed.trigger(HostEvent.TransformTableVizData, columnDataLite);
      * })
      * ```
-     * @version SDK: 1.35.12 | ThoughtSpot: 10.7.0.cl
+     * @version SDK: 1.38.0 | ThoughtSpot: 10.8.0.cl
      */
     TransformTableVizData = 'TransformTableVizData',
 }
@@ -3705,7 +3709,7 @@ export enum Action {
     * The **Collapse data panel** icon on the Search page.
     * Collapses the data panel view.
     *
-    * @version: SDK: 1.34.0 | ThoughtSpot Cloud: 10.3.0.cl, 10.7.0.sw
+    * @version: SDK: 1.34.0 | ThoughtSpot Cloud: 10.3.0.cl
     *
     * @example
     * ```js
@@ -4791,8 +4795,8 @@ export enum PrefetchFeatures {
 }
 
 /**
- * Enum for options to change context trigger
- * BOTH_CLICKS option is introduced in 10.7
+ * Enum for options to change context trigger.
+ * The `BOTH_CLICKS` option is available from 10.8.0.cl.
  */
 export enum ContextMenuTriggerOptions {
     LEFT_CLICK = 'left-click',
