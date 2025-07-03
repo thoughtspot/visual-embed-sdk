85c0217983e74bde9563e407850f94334983bdc6
sastaachar <justinjohnmathew100@gmail.com>
2025-07-03 19:37:08 +0530

    SCAL-258312 : test

--- src/embed/liveboard.ts
+++ src/embed/liveboard.ts
@@ -675,16 +675,24 @@ export class LiveboardEmbed extends V1Embed {
      */
     public destroy() {
         super.destroy();
-        if (this.viewConfig.fullHeight && this.viewConfig.lazyLoadingForFullHeight) {
-            window.removeEventListener('resize', this.sendFullHeightLazyLoadData.bind(this));
-            window.removeEventListener('scroll', this.sendFullHeightLazyLoadData.bind(this));
-        }
+        this.unregisterLazyLoadEvents();
     }
 
     private postRender() {
+        this.registerLazyLoadEvents();
+    }
+
+    private registerLazyLoadEvents() {
         if (this.viewConfig.fullHeight && this.viewConfig.lazyLoadingForFullHeight) {
-            window.addEventListener('resize', this.sendFullHeightLazyLoadData.bind(this));
-            window.addEventListener('scroll', this.sendFullHeightLazyLoadData.bind(this));
+            window.addEventListener('resize', this.sendFullHeightLazyLoadData);
+            window.addEventListener('scroll', this.sendFullHeightLazyLoadData);
+        }
+    }
+
+    private unregisterLazyLoadEvents() {
+        if (this.viewConfig.fullHeight && this.viewConfig.lazyLoadingForFullHeight) {
+            window.removeEventListener('resize', this.sendFullHeightLazyLoadData);
+            window.removeEventListener('scroll', this.sendFullHeightLazyLoadData);
         }
     }
 
