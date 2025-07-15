b8eaf1fc80a93e0430ef1ada63ab2cc5cd3d7e00
sastaachar <justinjohnmathew100@gmail.com>
2025-07-16 01:10:01 +0530

    SCAL-263562 : move alerts

--- src/types.ts
+++ src/types.ts
@@ -1010,6 +1010,22 @@ export interface BaseViewConfig {
      * @private
      */
     insertInToSlide?: boolean;
+    /**
+     * Show alert messages and toast messages in the embedded
+     * view in full app embed.
+     * 
+     * Supported embed types: `AppEmbed`
+     * @version SDK: 1.11.0 | ThoughtSpot: 8.3.0.cl, 8.4.1.sw
+     * @example
+     * ```js
+     * const embed = new AppEmbed('#tsEmbed', {
+     *    ... // other embed view config
+     *    showAlerts:true,
+     * })
+     * ```
+     */
+    showAlerts?: boolean;
+
 }
 
 /**
