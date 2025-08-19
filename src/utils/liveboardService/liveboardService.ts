6dbc03b86e5ef7d39ebbf11006727add7971a743
Aditya Mittal <114516106+adityamittal3107@users.noreply.github.com>
2025-08-19 11:32:36 +0530

    SCAL-267548 Improve documentation for CreateLiveboardWithAnswers (#276)

--- src/utils/liveboardService/liveboardService.ts
+++ src/utils/liveboardService/liveboardService.ts
@@ -4,16 +4,31 @@ import { getEmbedConfig } from '../../embed/embedConfig';
 import { executeTML } from '../../embed/base';
 
 /**
- * Create a liveboard with the given answers
- * @param answers AnswerService[]
- * @param name string
+ * Creates a new Liveboard in ThoughtSpot using the provided AnswerService instances.
+ * 
+ * Each answer will be added as a visualization to the newly created Liveboard.
+ * 
+ * @param {AnswerService[]} answers - An array of initialized `AnswerService` instances
+ * representing the answers to be added to the Liveboard.
+ * @param {string} name - The name of the Liveboard to create.
  * @returns result Promise
  * @version SDK: 1.33.1 | ThoughtSpot: *
  * @example
  * ```js
- *   const lb = createLiveboardWithAnswers([
- *      new AnswerService(session, null, 'tshost'),
- *   ], "my-liveboard");
+ * import { EmbedEvent, AnswerService } from "@thoughtspot/visual-embed-sdk";
+ *
+ * embed.on(EmbedEvent.Data, async () => {
+ *   try {
+ *     const answerService = await embed.getAnswerService();
+ *     const lb = await createLiveboardWithAnswers(
+ *       [answerService],
+ *       "My Liveboard"
+ *     );
+ *     console.log("Liveboard created:", lb);
+ *   } catch (err) {
+ *     console.error("Failed to create liveboard:", err);
+ *   }
+ * });
  * ```
  */
 export const createLiveboardWithAnswers = async (
