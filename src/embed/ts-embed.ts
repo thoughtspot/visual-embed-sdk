ae1a09e544b9e5d454fddb49a1d021e4c6ae0397
shivam-kumar <shivam.kumar@thoughtspot.com>
2025-11-11 18:20:58 +0530

    SCAL-239528 refactor getBlockedAndAllowedRoutes to filter out empty routes

--- src/embed/ts-embed.ts
+++ src/embed/ts-embed.ts
@@ -461,8 +461,18 @@ export class TsEmbed {
                     error : { type: EmbedErrorCodes.CUSTOM_ACTION_VALIDATION, message: customActionsResult.errors }
                 });
         }
-        const blockedAndAllowedRoutesResult = getBlockedAndAllowedRoutes( this.viewConfig?.routeBlocking, { embedComponentType: (this.viewConfig as any).embedComponentType || '', liveboardId: (this.viewConfig as any).liveboardId, vizId: (this.viewConfig as any).vizId, activeTabId: (this.viewConfig as any).activeTabId, pageId: (this.viewConfig as any).pageId, path: (this.viewConfig as any).path });
-        if(blockedAndAllowedRoutesResult.error) {
+        const blockedAndAllowedRoutesResult = getBlockedAndAllowedRoutes(
+            this.viewConfig?.routeBlocking,
+            {
+                embedComponentType: (this.viewConfig as any).embedComponentType || '',
+                liveboardId: (this.viewConfig as any).liveboardId,
+                vizId: (this.viewConfig as any).vizId,
+                activeTabId: (this.viewConfig as any).activeTabId,
+                pageId: (this.viewConfig as any).pageId,
+                path: (this.viewConfig as any).path,
+            },
+        );
+        if (blockedAndAllowedRoutesResult.error) {
             this.handleError(blockedAndAllowedRoutesResult.message);
         }
         const baseInitData = {
