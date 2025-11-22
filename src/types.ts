f53ed6ff836edf3d2b8549b6740a5ae5cf050258
shivam-kumar <shivam.kumar@thoughtspot.com>
2025-11-22 14:28:15 +0530

    SCAL-256912 Enhance error handling for API intercept body parsing with standardized error

--- src/types.ts
+++ src/types.ts
@@ -6269,6 +6269,9 @@ export enum EmbedErrorCodes {
     
     /** Host event type is undefined or invalid */
     HOST_EVENT_TYPE_UNDEFINED = 'HOST_EVENT_TYPE_UNDEFINED',
+
+    /** Error parsing api intercept body */
+    PARSING_API_INTERCEPT_BODY_ERROR = 'PARSING_API_INTERCEPT_BODY_ERROR',
 }
 
 /**
