22b2c7a746607f692af299b20ea0fab05398617c
Ruchi Anand <ruchi.anand@thoughtspot.com>
2025-12-10 11:24:37 +0530

    changed to logger

--- src/embed/liveboard.ts
+++ src/embed/liveboard.ts
@@ -529,10 +529,10 @@ export class LiveboardEmbed extends V1Embed {
             if (this.viewConfig.lazyLoadingForFullHeight) {
                 params[Param.IsLazyLoadingForEmbedEnabled] = true;
                 if (this.viewConfig.lazyLoadingMargin === undefined) {
-                    console.warn('Please provide CSS margin string value (e.g., "10px"). Defaulting to "0px".');
+                    logger.warn('Please provide CSS margin string value (e.g., "10px"). Defaulting to "0px".');
                     params[Param.RootMarginForLazyLoad] = '0px';
                 } else if (typeof this.viewConfig.lazyLoadingMargin !== 'string' || !isValidCssMargin(this.viewConfig.lazyLoadingMargin)) {
-                    console.error('lazyLoadingMargin must be a valid CSS margin string value (e.g., "10px"). Defaulting to "0px".');
+                    logger.error('lazyLoadingMargin must be a valid CSS margin string value (e.g., "10px"). Defaulting to "0px".');
                     params[Param.RootMarginForLazyLoad] = '0px';
                 } else {
                     params[Param.RootMarginForLazyLoad] = this.viewConfig.lazyLoadingMargin;
