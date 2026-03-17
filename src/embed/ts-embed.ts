453e2cd30f1b093c3a4efa25e323d139b1d604e0
Ruchi Anand <ruchi.anand@thoughtspot.com>
2026-03-17 15:31:18 +0530

    test

--- src/embed/ts-embed.ts
+++ src/embed/ts-embed.ts
@@ -1461,7 +1461,7 @@ export class TsEmbed {
                     code: EmbedErrorCodes.UPDATEFILTERS_INVALID_PAYLOAD,
                     error: err.message,
                 };
-                this.executeCallbacks(EmbedEvent.Error, errorDetails, this.eventPort);
+                this.executeCallbacks(EmbedEvent.Error, {eventType: EmbedEvent.Error, data: errorDetails}, this.eventPort);
             }
             throw err;
         });
